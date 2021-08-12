009fad50b7ecaf4dfca9ba5e2d3c592aa72eff48
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-08-12 16:36:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ img = "";
 status = "";
 object = [] ;
 function setup() {
-    canvas = createCanvas(640,420);
+    canvas = createCanvas(380,380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
     objectDetector = ml5.objectDetector("cocossd" , modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
@@ -15,28 +18,34 @@ function preload()
 
 function draw()
     {
-        image(img,0,0,640,420);
+        image(video,0,0,380,380);
         if(status != "")
         {
+            r = random(255);
+            g = random(255);
+            b = random(255);
+            objectDetector.detect(video,gotResult);
             for(i = 0; i < object.length ; i++)
             {
-                document.getElementById("status").innetatusrHTML = "Status : Object Detected";
-                fill("red");
+                document.getElementById("status").innerHTML = "Status : Object Detected";
+                document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+object.length;
+                fill(r,g,b);
                 percent = floor(object[i].confidence * 100);
                 text(object[i].label + " " + percent + "%", object[i].x , object[i].y);
                 noFill();
-                stroke("blue");
+                stroke(r,g,b);
                 rect(object[i].x,object[i].y -10, object[i].width , object[i].height);
             }
         }
       
+        
     }
 
     function modelLoaded()
     {
         console.log("Model is Loaded");
         status = true;
-        objectDetector.detect(img,gotResult);
+        
     }
 
     function gotResult(error,results)
